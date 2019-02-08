46d447b815e60c83c3c74433674c4c92862db72b
epietras220 <epietras220@users.noreply.github.com>
2019-02-08 13:04:04 +0100

    as

---
+++ src/main/java/com/epietras/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.epietras.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
+
